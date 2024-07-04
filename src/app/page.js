3702cfdb20e57c2e284b32e83978ff1529cf3df7
unknown <redoyhasan4585@gmail.com>
2024-07-04 15:08:42 +0400

    working on navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main className="">
-   
-
+     <button className="btn btn-primary">hello</button>
     </main>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,74 @@
+import Image from "next/image";
+import logo from "../../app/favicon.ico"
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="navbar bg-base-200  max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto">
+    <div className="navbar-start">
+      <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h7" />
+          </svg>
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li><a>Homepage</a></li>
+          <li><a>Portfolio</a></li>
+          <li><a>About</a></li>
+        </ul>
+      </div>
     </div>
+    <div className="navbar-center">
+     <Link href={'/'} className=" flex gap-5 items-center bg-primary ">
+     <Image src={logo} alt="logo" width={40} height={40}></Image>
+     <h4 className=" text-xl font-bold">Poultry Hub</h4></Link>
+    </div>
+    <div className="navbar-end">
+      <button className="btn btn-ghost btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+      </button>
+      <button className="btn btn-ghost btn-circle">
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+          </svg>
+          <span className="badge badge-xs badge-primary indicator-item"></span>
+        </div>
+      </button>
+    </div>
+  </div>
   );
 };
 
