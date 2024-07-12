1ac1307fbcb4da42a9734aebce7f8941336ecf71
unknown <redoyhasan4585@gmail.com>
2024-07-12 21:17:54 +0400

    working on our partnern section try to implement slider

--- src/Pages/Home/OurPartners/OurPartners.jsx
+++ src/Pages/Home/OurPartners/OurPartners.jsx
@@ -1,21 +1,73 @@
 import Heading from "@/components/custom/Heading/Heading";
 import useGetData from "@/hooks/useGetData";
+import Image from "next/image";
+import Link from "next/link";
+import { FaLocationArrow } from "react-icons/fa";
+import { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+import { FreeMode, Pagination } from "swiper/modules";
 
 const OurPartners = () => {
-  const partners = useGetData('/api/partners')
-  console.log(partners);
+  const partners = useGetData("/api/partners");
+  // console.log(partners[0]);
   return (
     <main className="my-20 max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto ">
-      <Heading subHeading={'Our Partners'} title={'Here all of our loved Partners'}></Heading>
-      <section>
-        {partners?.map(partner=><div key={partner?._id} className="flex flex-col max-w-md p-6 ">
-	<img src="https://source.unsplash.com/200x200/?portrait?2" alt="" className="flex-shrink-0 object-cover h-64 rounded-sm sm:h-96  aspect-square" />
-	<div>
-		<h2 className="text-xl font-semibold">Leroy Jenkins</h2>
-		<span className="block pb-2 text-sm">CTO of Company Inc.</span>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, eligendi dolorum sequi illum qui unde aspernatur non deserunt</p>
-	</div>
-</div>)}
+      <Heading
+        subHeading={"Our Partners"}
+        title={"Here all of our loved Partners"}
+      ></Heading>
+      <section className=" grid grid-cols-2 gap-5 lg:grid-cols-3 mt-10 ">
+      <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+        >
+        {partners[0]?.map((partner) => (
+            <SwiperSlide>
+               <div
+            key={partner?._id}
+            className="flex relative flex-col p-3 bg-base-300 shadow rounded-lg "
+          >
+            <div className=" px-5 mb-5">
+              <Image
+                src={partner?.image}
+                width={200}
+                height={200}
+                alt="partner image"
+                className=" rounded-lg bg-white w-full h-28 max-h-28 min-h-28 "
+              />
+            </div>
+            <div>
+              <h2 className="text-xl font-semibold">{partner?.name}</h2>
+              <h6>{partner?.description}</h6>
+            </div>
+            <div className=" flex justify-end bottom-1 absolute right-1  ">
+              <Link
+                title="Visit"
+                className=" hover:cursor-pointer hover:bg-slate-100 p-2 rounded-full"
+                href={partner?.website}
+              >
+                <FaLocationArrow className=" text-yellow-500 text-2xl"></FaLocationArrow>
+              </Link>
+            </div>
+          </div></SwiperSlide>
+        ))}
+        </Swiper>
       </section>
     </main>
   );
