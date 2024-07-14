207413702f37f16994b310452c4344b1c3e2692b
unknown <redoyhasan4585@gmail.com>
2024-07-14 11:44:50 +0400

    working on register added regsiter from

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import login_banner from '../../../public/login-image.png';
+import signUp_banner from '../../../public/sign-up.png';
 import React from 'react';
 import Link from 'next/link';
 
@@ -7,18 +7,34 @@ import Link from 'next/link';
 const Register = () => {
   return (
     <main className="hero  min-h-screen  max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto">
-  <div className="hero-content flex-col lg:flex-row">
+  <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center flex-1 lg:text-left">
-      <Image src={login_banner} alt='login banner imge' height={700} width={700}/>
+      <Image src={signUp_banner} alt='login banner imge' height={700} width={700}/>
     </div>
-    <div className="flex-1 card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="flex-1 card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
       <form className="card-body">
+        {/* Name div  */}
+        <div className="form-control">
+          <label className="label">
+            <span className="subtitle ">Full Name</span>
+          </label>
+          <input type="text" placeholder="Full Name" className="input input-bordered" required />
+        </div>
+        {/* Phone number div  */}
+        <div className="form-control">
+          <label className="label">
+            <span className="subtitle ">Phone Number</span>
+          </label>
+          <input type="number" placeholder="Your Phone Number" className="input input-bordered" required />
+        </div>
+        {/* email div  */}
         <div className="form-control">
           <label className="label">
             <span className="subtitle ">Email</span>
           </label>
           <input type="email" placeholder="email" className="input input-bordered" required />
         </div>
+        {/* password div  */}
         <div className="form-control">
           <label className="label">
             <span className="subtitle ">Password</span>
@@ -28,8 +44,13 @@ const Register = () => {
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
+        {/* check box div  */}
+        <div  className=' flex gap-2' >
+        <input type="checkbox" name="remember" aria-label="bbb" className="mr-1 rounded-sm focus:dark:ring-violet-600 focus:dark:border-violet-600 focus:ring-2 dark:accent-violet-600" />
+        <h6 className=' text-sm'>Accept Our Terms & Conditions </h6>
+        </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary">SignUp</button>
         </div>
       </form>
       <Link href={'/login'} className=' py-3 flex  justify-center gap-2  text-lg text-center font-medium'>Already Poultry Hub member? Please  <span className=' font-bold text-blue-700'>Login</span>.</Link>
