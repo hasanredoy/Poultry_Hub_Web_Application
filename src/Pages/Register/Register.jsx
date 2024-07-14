50c4a1e3918f3d201462ace7f04b89fda29f5992
unknown <redoyhasan4585@gmail.com>
2024-07-14 15:04:58 +0400

    make some changes in login page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -9,44 +9,42 @@ const Register = () => {
     <main className="hero  min-h-screen  max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className=" flex-1 lg:text-left w-full min-h-screen h-full">
-      <Image src={signUp_banner} className=' w-full h-full' alt='login banner imge' height={700} width={800}/>
+      <Image src={signUp_banner} className=' h-[400px] w-full lg:h-full' alt='login banner imge' height={700} width={800}/>
     </div>
-    <div className="flex-1 card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
-      <h3 className=' text-center subtitle pt-3'>Please Register</h3>
+    <div className="flex-1 card bg-base-200 w-full max-w-2xl lg:max-w-md  ">
+      <h3 className=' text-center text-base font-bold lg:text-xl pt-3'>Please Register</h3>
       <form className="card-body">
         {/* Name div  */}
         <div className="form-control">
           <label className="label">
-            <span className=" description ">Full Name</span>
+            <span className="   text-sm font-bold lg:text-base ">Full Name</span>
           </label>
           <input type="text" placeholder="Full Name" className="input input-bordered" required />
         </div>
         {/* Phone number div  */}
         <div className="form-control">
           <label className="label">
-            <span className=" description ">Phone Number</span>
+            <span className="   text-sm font-bold lg:text-base ">Phone Number</span>
           </label>
           <input type="number" placeholder="Your Phone Number" className="input input-bordered" required />
         </div>
         {/* email div  */}
         <div className="form-control">
           <label className="label">
-            <span className=" description ">Email</span>
+            <span className="   text-sm font-bold lg:text-base ">Email</span>
           </label>
           <input type="email" placeholder="Email" className="input input-bordered" required />
         </div>
         {/* password div  */}
         <div className="form-control">
           <label className="label">
-            <span className=" description ">Password</span>
+            <span className="   text-sm font-bold lg:text-base ">Password</span>
           </label>
           <input type="password" placeholder="Password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+         
         </div>
         {/* check box div  */}
-        <div  className=' flex gap-2' >
+        <div  className=' flex gap-2 mt-3' >
         <input type="checkbox" name="remember" aria-label="bbb" className="mr-1 rounded-sm focus:dark:ring-violet-600 focus:dark:border-violet-600 focus:ring-2 dark:accent-violet-600" />
         <h6 className=' text-sm'>Accept Our Terms & Conditions </h6>
         </div>
@@ -54,7 +52,7 @@ const Register = () => {
           <button className="btn btn-primary">SignUp</button>
         </div>
       </form>
-      <Link href={'/login'} className=' py-3 flex  justify-center gap-2  text-lg text-center font-medium'>Already Poultry Hub member? Please  <span className=' font-bold text-blue-700'>Login.</span></Link>
+      <Link href={'/login'} className=' py-3 flex  justify-center gap-2  text-sm lg:text-base text-center font-medium'>Already Poultry Hub member? Please  <span className=' font-bold text-blue-700'>Login.</span></Link>
     </div>
   </div>
 
