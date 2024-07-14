b039c9800979b98e3302b58f56b96a9eea7752f9
unknown <redoyhasan4585@gmail.com>
2024-07-14 14:58:10 +0400

    make some changes in login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,23 +7,23 @@ import Link from 'next/link';
 const Login = () => {
   return (
     <main className="hero  min-h-screen  max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto">
-  <div className="hero-content flex-col lg:flex-row">
+  <div className="hero-content flex-col md:flex-row">
     <div className="text-center flex-1 lg:text-left">
-      <Image src={login_banner} alt='login banner imge' height={700} width={700}/>
+      <Image src={login_banner} className=' h-[300px] lg:h-full' alt='login banner imge' height={700} width={700}/>
     </div>
-    <div className="flex-1 card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="flex-1 card bg-base-200 w-full max-w-sm shrink-0 ">
     <h3 className=' text-center subtitle pt-3'>Please Login</h3>
 
       <form className="card-body">
         <div className="form-control">
           <label className="label">
-            <span className=" description ">Email</span>
+            <span className=" text-sm font-bold lg:text-lg  ">Email</span>
           </label>
           <input type="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className=" description ">Password</span>
+            <span className=" text-sm font-bold lg:text-lg  ">Password</span>
           </label>
           <input type="password" placeholder="password" className="input input-bordered" required />
           <label className="label">
@@ -31,10 +31,10 @@ const Login = () => {
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className=" btn btn-primary ">Login</button>
         </div>
       </form>
-      <Link href={'/register'} className=' py-3 flex  justify-center gap-2  text-lg text-center font-medium'>New in Poultry Hub? Please  <span className=' font-bold text-blue-700'>Register</span>.</Link>
+      <Link href={'/register'} className=' py-3 flex  justify-center gap-2 text-sm lg:text-lg text-center font-medium'>New in Poultry Hub? Please  <span className=' font-bold text-blue-700'>Register</span>.</Link>
     </div>
   </div>
 
