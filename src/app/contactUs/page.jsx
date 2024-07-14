83fe0ac563ca7eec27eaa4f18cb262b24780ea08
unknown <redoyhasan4585@gmail.com>
2024-07-14 22:21:45 +0400

    working on contact us page added send message form

--- src/app/contactUs/page.jsx
+++ src/app/contactUs/page.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import contactUs from "../../../public/contactus/contact_us.png";
 import messageImage from '../../../public/contactus/message.png'
 import Heading from "@/components/custom/Heading/Heading";
+import { FaLocationArrow } from "react-icons/fa";
 const ContactUs = () => {
   return (
     <main className=" my-28 max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto">
@@ -27,32 +28,31 @@ const ContactUs = () => {
 {/* message section  */}
       <section className=" my-10 ">
         <Heading subHeading={'Feel Free '} title={'Send Us Direct Message'}></Heading>
-      <div className="hero-content flex-col lg:flex-row">
+      <div className="hero-content justify-between flex-col lg:flex-row">
     <Image
      alt="message us image"
-     height={600} 
-     width={600}
+     height={400} 
+     width={400}
       src={messageImage}
       className="max-w-md rounded-lg " />
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="card bg-base-200 w-full max-w-md shrink-0 ">
+      <h3 className="subtitle text-center pt-2">Leave a Message</h3>
       <form className="card-body">
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="text-sm lg:text-base font-bold">Subject</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="text" placeholder="subject" className="input " required />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="text-sm lg:text-base font-bold">Message</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+          <textarea name="message" className=" textarea " id=""></textarea>
+         
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn flex items-center gap-2 btn-primary">Send <FaLocationArrow></FaLocationArrow></button>
         </div>
       </form>
     </div>
