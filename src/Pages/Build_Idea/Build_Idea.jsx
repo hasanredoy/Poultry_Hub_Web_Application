c17618fc76feecab9bc22d34fa12b8c533f21606
unknown <redoyhasan4585@gmail.com>
2024-07-19 20:01:40 +0400

    working on build_idea page

--- src/Pages/Build_Idea/Build_Idea.jsx
+++ src/Pages/Build_Idea/Build_Idea.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import bannerImage from '../../../public/generateIdea/ai-cloud-concept-with-robot-arm.jpg'
 import { FaLocationArrow } from 'react-icons/fa';
+import aiIcon from '../../../public/generateIdea/chatbot.png'
+
 const Build_Idea = () => {
   return (
     <main className='my-28 max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto flex gap-10 '>
@@ -21,13 +23,20 @@ const Build_Idea = () => {
             <button className='btn-prompt'>How much money do I need to build a medium poultry feed factory?</button>
 
          </div>
-         <div className="join mt-14 ">
-        <input
-          type="text"
-          placeholder="Ask Anything"
-          className=" px-3 border border-gray-400 outline-none join-item w-full min-w-full " />
-        <button className="btn border-2 btn-primary join-item  flex items-center gap-2">Ask<FaLocationArrow className=' -rotate-45'></FaLocationArrow></button>
-      </div>
+ {/* input section  */}
+     <section className=' mt-14 flex gap-6 items-center justify-center'>
+     <div className=' ='>
+     <Image src={aiIcon} width={40} height={40} alt='ai chatbot icon' className=' '></Image>
+     </div>
+     <div className="join flex-1">
+       
+       <input
+         type="text"
+         placeholder="Ask Anything"
+         className=" px-3 border border-gray-400 outline-none join-item w-full min-w-full " />
+       <button className="btn border-2 btn-primary join-item  flex items-center gap-2">Ask<FaLocationArrow className=' -rotate-45'></FaLocationArrow></button>
+     </div>      
+            </section> 
       </section>
     </main>
   );
