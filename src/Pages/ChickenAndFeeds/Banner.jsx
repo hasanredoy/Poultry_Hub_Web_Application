a157ce85d3dcb66e9971c85d3e373d1fbd3214ea
unknown <redoyhasan4585@gmail.com>
2024-07-21 20:31:26 +0400

    added banner image  in chicken and feeds page

--- src/Pages/ChickenAndFeeds/Banner.jsx
+++ src/Pages/ChickenAndFeeds/Banner.jsx
@@ -3,24 +3,24 @@
 const Banner = () => {
   return (
     <section
-    className="hero min-h-screen bg-cover"
+    className="hero min-h-[calc(100dvh-100px)] bg-cover"
     style={{
-      backgroundImage:`url(https://i.postimg.cc/28dw0cjr/78312775-l8vo-xcof-230925.jpg)`,
+      backgroundImage:`url(https://i.postimg.cc/Hn9y9NwN/Untitled-design-7.png)`,
     }}>
      
-    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero-overlay bg-black bg-opacity-50 dark:bg-white"></div>
     <div className="hero-content text-neutral-content text-center">
-      <div className="max-w-md">
-        <h1 className="mb-5 text-5xl font-bold">Welcome To <span className=" text-primary">Poultry Hub</span></h1>
-        <p className="mb-5">
+      <div className="max-w-xl">
+        <h1 className="mb-5 text-white text-5xl font-bold">Welcome To <span className=" text-primary">Poultry Hub</span></h1>
+        <p className="mb-5 text-white">
          Hello there, we are providing fresh chicken from 100% organic farms. And we're also selling poultry feed that came from organic factories.
         </p>
         <div className="join">
         <input
           type="text"
-          placeholder="username@site.com"
-          className="input input-bordered join-item" />
-        <button className="btn btn-primary join-item">Subscribe</button>
+          placeholder="search your wisdom.."
+          className="input input-bordered join-item bg-white text-black font-bold" />
+        <button className="btn btn-primary join-item">Search</button>
       </div>
       </div>
     </div>
--- src/Pages/ChickenAndFeeds/ChickenAndFeeds.jsx
+++ src/Pages/ChickenAndFeeds/ChickenAndFeeds.jsx
@@ -1,38 +1,62 @@
-'use client'
+"use client";
 import useAxios from "@/hooks/useAxios";
 import { useEffect, useState } from "react";
 import Banner from "./Banner";
+import Heading from "@/components/custom/Heading/Heading";
 
 // get custom axios hook
-const axiosHook =useAxios()
+const axiosHook = useAxios();
 // load all chicken and feeds data
-const loadAllItems =async()=>{
-  const res =await axiosHook.get(`/api/all_items`)
+const loadAllItems = async () => {
+  const res = await axiosHook.get(`/api/all_items`);
   // console.log(res?.data?.result);
-  return res?.data?.result
-}
+  return res?.data?.result;
+};
 const ChickenAndFeeds = () => {
-  // state for all chicken and feed 
-  const [allChickenAndFeeds, setAllChickenAndFeeds]= useState([])
+  // state for all chicken and feed
+  const [allChickenAndFeeds, setAllChickenAndFeeds] = useState([]);
 
-  useEffect(()=>{
-    //function for call loadAllItems 
-    const loader=async()=>{
-      const data =await loadAllItems()
+  useEffect(() => {
+    //function for call loadAllItems
+    const loader = async () => {
+      const data = await loadAllItems();
       // console.log({data});
-    setAllChickenAndFeeds(data)
-    } 
-    loader()
-  },[])
+      setAllChickenAndFeeds(data);
+    };
+    loader();
+  }, []);
   // console.log({allChickenAndFeeds});
 
   return (
-    <main >
+    <main>
       {/* banner  */}
-      <section >
+      <section>
         <Banner></Banner>
       </section>
-   
+      {/* filter and heading section */}
+      <section className=" my-28 max-w-[95%] lg:max-w-[85%] mx-auto">
+        <Heading
+          subHeading={"Welcome"}
+          title={"Buy your favorite item. "}
+        ></Heading>
+        {/* filter drop down  */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-primary m-1">
+            Filter By
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-[40] w-52 p-2 shadow"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 };
