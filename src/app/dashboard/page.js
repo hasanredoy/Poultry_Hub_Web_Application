52d8f57dac5f2768a59499a413ecfee124c5d753
unknown <redoyhasan4585@gmail.com>
2024-07-23 23:28:22 +0400

    working profile page added proflie pic name and edit button

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { FaPen } from "react-icons/fa";
 
 const page = () => {
 //  get user 
@@ -10,13 +11,22 @@ const page = () => {
   
   }
   return (
-    <section>
+    <section className=" flex flex-col justify-center my-20 ">
       {/* avatar section  */}
       <section className=" mx-auto">
       <div className="avatar">
-  <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-    <Image alt=" avatar" width={40} height={50} src={user?.image} />
+  <div className="ring-success ring-offset-base-100 w-36 rounded-full ring ring-offset-2">
+    <Image className=" w-36 h-36" alt=" avatar" width={200} height={200} src={user?.image} />
   </div>
+</div>
+<div className=" flex flex-col gap-3 justify-center text-center my-6">
+<h3  className=" text-xl font-bold text-primary">
+  {user?.name}
+</h3>
+<button className=" btn btn-neutral flex items-center gap-3">
+      <FaPen></FaPen>
+      Update Profile
+    </button>
 </div>
       </section>
     </section>
