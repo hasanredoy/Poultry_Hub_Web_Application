8d6e0ba533501c9c26d0372a92960a22c55470f8
unknown <redoyhasan4585@gmail.com>
2024-07-24 19:12:25 +0400

    working on user home

---
+++ src/app/dashboard/user_home/page.jsx
@@ -0,0 +1,25 @@
+
+const page = () => {
+    //  get user
+    const user = {
+      name: "Mr X",
+      email: "hello@gmail.com",
+      phone: "+934990898",
+      image: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    };
+  return (
+    <main className=" flex gap-10 flex-col lg:flex-row">
+      c
+      {/* user info section */}
+      <section>
+
+      </section>
+      {/* user order info  section */}
+      <section>
+
+      </section>
+    </main>
+  );
+};
+
+export default page;
