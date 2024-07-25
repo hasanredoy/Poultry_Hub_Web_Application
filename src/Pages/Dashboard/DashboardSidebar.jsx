e663c69de6d894bbf766a6601b74900946ce8b92
unknown <redoyhasan4585@gmail.com>
2024-07-25 13:56:32 +0400

    working on seller home

--- src/Pages/Dashboard/DashboardSidebar.jsx
+++ src/Pages/Dashboard/DashboardSidebar.jsx
@@ -1,27 +1,4 @@
-"use client";
-import {
-  FaHistory,
-  FaHome,
-  FaLightbulb,
-  FaList,
-  FaMoon,
-  FaPlus,
-  FaShoppingCart,
-  FaSignOutAlt,
-  FaStar,
-  FaUser,
-  FaUsers,
-} from "react-icons/fa";
-import { MdOutlineContactSupport, MdPersonAdd } from "react-icons/md";
-import { TiThMenu } from "react-icons/ti";
-import { LuMonitorSmartphone } from "react-icons/lu";
-import { IoSunnySharp } from "react-icons/io5";
-import { MdAddShoppingCart } from "react-icons/md";
-import { AiFillProduct } from "react-icons/ai";
-import { usePathname } from "next/navigation";
-import { IoIosMoon, IoIosSunny } from "react-icons/io";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+
 import LinksForLargeDevice from "./NavLinks/LinksForLargeDevice";
 import LinksForSmallDevice from "./NavLinks/LinksForSmallDevice";
 
