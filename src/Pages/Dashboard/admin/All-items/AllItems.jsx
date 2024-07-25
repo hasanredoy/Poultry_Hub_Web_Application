bc908c7c6bb6d72aad9d1dd70b8ff01954e8800f
unknown <redoyhasan4585@gmail.com>
2024-07-25 19:42:21 +0400

    working on all items page loaded all data

--- src/Pages/Dashboard/admin/All-items/AllItems.jsx
+++ src/Pages/Dashboard/admin/All-items/AllItems.jsx
@@ -1,9 +1,34 @@
+'use client'
+import useAxios from "@/hooks/useAxios";
+import { useEffect, useState } from "react";
 
+// get custom axios hook
+const axiosHook = useAxios();
+// load all chicken and feeds data
+const loadAllItems = async () => {
+  const res = await axiosHook.get(`/api/all_items`);
+  // console.log(res?.data?.result);
+  return res?.data?.result;
+};
 const AllItems = () => {
+  // state for all chicken and feed
+  const [allChickenAndFeeds, setAllChickenAndFeeds] = useState([]);
+
+  useEffect(() => {
+    //function for call loadAllItems
+    const loader = async () => {
+      const data = await loadAllItems();
+      console.log(data);
+      setAllChickenAndFeeds(data);
+    };
+    loader();
+  }, []);
+  console.log({allChickenAndFeeds});
+  
   return (
-    <div>
+    <main>
       
-    </div>
+    </main>
   );
 };
 
---
+++ src/app/dashboard/all_items/page.jsx
@@ -0,0 +1,9 @@
+import AllItems from "@/Pages/Dashboard/admin/All-items/AllItems";
+
+const page = () => {
+  return (
+    <AllItems></AllItems>
+  );
+};
+
+export default page;
