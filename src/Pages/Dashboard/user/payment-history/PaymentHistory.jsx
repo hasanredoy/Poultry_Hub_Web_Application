34e00285834353f5c97ed375a621628f278fdc1c
unknown <redoyhasan4585@gmail.com>
2024-07-25 13:02:13 +0400

    working on payment history page

---
+++ src/Pages/Dashboard/user/payment-history/PaymentHistory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentHistory = () => {
+  return (
+    <main>
+      
+    </main>
+  );
+};
+
+export default PaymentHistory;
---
+++ src/app/dashboard/payment_history/page.jsx
@@ -0,0 +1,9 @@
+import PaymentHistory from "@/Pages/Dashboard/user/payment-history/PaymentHistory";
+
+const page = () => {
+  return (
+   <PaymentHistory></PaymentHistory>
+  );
+};
+
+export default page;
