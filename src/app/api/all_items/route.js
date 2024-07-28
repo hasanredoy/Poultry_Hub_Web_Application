286407ea5bde58e51906ea841fcd0393f6831b19
unknown <redoyhasan4585@gmail.com>
2024-07-28 12:19:40 +0400

    fix: that issue was i was trying to give 2 different queries for search and filter now i combined them and ist working

--- src/app/api/all_items/route.js
+++ src/app/api/all_items/route.js
@@ -1,41 +1,40 @@
-import { connectDB } from "@/lib/connectDB"
+import { connectDB } from "@/lib/connectDB";
 import { NextResponse } from "next/server";
 
-export const GET=async(request)=>{
-  // get query 
-  const queryFilter = await request.nextUrl.searchParams.get('filter')
-  const search = await request.nextUrl.searchParams.get('search')
-  
-  const page = await request.nextUrl.searchParams.get('page')
-  
-  const size = await request.nextUrl.searchParams.get('size')
-  console.log({queryFilter});
-  const parsedPage = parseInt(page)
-  const parsedSize = parseInt(size)
-  
+export const GET = async (request) => {
+  // get query
+  const queryFilter = await request.nextUrl.searchParams.get("filter");
+  const search = await request.nextUrl.searchParams.get("search");
+
+  const page = (await request.nextUrl.searchParams.get("page")) || "0";
+
+  const size = (await request.nextUrl.searchParams.get("size")) || "8";
+  console.log({ queryFilter });
+  const parsedPage = parseInt(page);
+  const parsedSize = parseInt(size);
+
   try {
-    const db = await connectDB()
-    const itemsCollection = await db.collection('All_Items')
-    let query ={}
-   let filter = {}
-    if(queryFilter){
-      filter = {category:queryFilter}
-    }  
-   
-    // check if any search query then set query search value
-    if(search){
-      query = {
-        $or:[
-          {title:{$regex:search,$options:'i'}},
-          {description:{$regex:search,$options:'i'}},
-        ]
-      }
-    }  
-    const result = await itemsCollection.find(query).limit(parsedSize).skip(parsedPage*parsedSize).toArray()
+    const db = await connectDB();
+    const itemsCollection = await db.collection("All_Items");
+    let query = {};
+    // Combine filter and search queries
+    if (queryFilter) {
+      query.category = queryFilter;
+    }
+    if (search) {
+      query.$or = [
+        { title: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+    const result = await itemsCollection
+      .find(query)
+      .limit(parsedSize)
+      .skip(parsedPage * parsedSize)
+      .toArray();
     // console.log({result},'from server');
-    return NextResponse.json({result}) 
+    return NextResponse.json({ result });
   } catch (error) {
-    return NextResponse.json({error}) 
-    
+    return NextResponse.json({ error });
   }
-}
+};
