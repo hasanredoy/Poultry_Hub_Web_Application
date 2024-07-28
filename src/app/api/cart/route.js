bf6a9c618782089ebed16298dbb43bb2237c4ea4
unknown <redoyhasan4585@gmail.com>
2024-07-28 22:28:24 +0400

    loading cart

--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -3,12 +3,14 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   // get query
-
+  const email = await request.nextUrl.searchParams.get('email')
   try {
     const db = await connectDB();
     const cartsCollection = await db.collection("carts");
     let query = {};
-
+  if(email){
+    query={email}
+  }
     const result = await cartsCollection
       .find(query)
       .toArray();
---
+++ src/hooks/useUserCart.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import useAxios from "./useAxios";
+import useGetUser from "./useGetUser";
+
+const axiosHook = useAxios()
+// function to load cart
+const loadCart= async(email)=>{
+ const {data}=await axiosHook.get(`/api/cart?email=${email}`)
+ console.log(data,'cart');
+ return data?.result
+}
+const useUserCart = () => {
+  // cart state
+  const [cart,setCart]=useState([])
+  // get user 
+  const user = useGetUser()
+  useEffect(()=>{
+    const loader = async()=>{
+      const cartData = await loadCart(user.email)
+      setCart(cartData)
+    }
+    loader()
+  },[])
+ console.log({cart});
+  return [cart]
+};
+
+export default useUserCart;
