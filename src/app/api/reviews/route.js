27f9974eb12d4f84e054348b8b8aa0179f6f3249
unknown <redoyhasan4585@gmail.com>
2024-07-30 14:00:11 +0400

    working on review page

--- src/app/api/reviews/route.js
+++ src/app/api/reviews/route.js
@@ -7,7 +7,22 @@ export const GET=async()=>{
     // console.log('hello');
   const reviewsCollections = await db.collection('reviews')
   // console.log({reviewsCollections});
-  const result = await reviewsCollections.find().toArray()
+  const result = await reviewsCollections.find({postedDate:-1}).toArray()
+  // console.log({result},'hello2');
+  return NextResponse.json(result?{result}:{})
+} catch (error) {
+   return NextResponse.json({error})
+  
+ }
+}
+export const POST=async(request)=>{
+  const reviewData = await request.json()
+  try {
+    const db = await connectDB()
+    // console.log('hello');
+  const reviewsCollections = await db.collection('reviews')
+  // console.log({reviewsCollections});
+  const result = await reviewsCollections.insertOne(reviewData)
   // console.log({result},'hello2');
   return NextResponse.json(result?{result}:{})
 } catch (error) {
