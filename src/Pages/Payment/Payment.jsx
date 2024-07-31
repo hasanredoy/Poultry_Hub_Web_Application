086f6d100659df72b22d8d20888a32fe69c62aba
unknown <redoyhasan4585@gmail.com>
2024-07-31 11:50:51 +0400

    loaded stipe in payment page now working on checkout form

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,9 +1,16 @@
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import CheckoutForm from './checkOutForm/CheckoutForm';
+
+// load stipe with pk
+const stripePromise = loadStripe(process.env.NEXT_STRIPE_PK);
+console.log({pk:process.env.NEXT_STRIPE_PK});
 
 const Payment = () => {
   return (
-    <div>
-      
-    </div>
+    <Elements stripe={stripePromise} options={options}>
+    <CheckoutForm />
+  </Elements>
   );
 };
 
---
+++ src/Pages/Payment/checkOutForm/CheckoutForm.jsx
@@ -0,0 +1,11 @@
+import {PaymentElement} from '@stripe/react-stripe-js';
+
+const CheckoutForm = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default CheckoutForm;
