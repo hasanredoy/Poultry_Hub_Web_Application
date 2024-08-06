9d40906b813f84e4116a62c1937581b75b38b4c1
unknown <redoyhasan4585@gmail.com>
2024-08-06 19:35:51 +0400

    added loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import { ImSpinner9 } from "react-icons/im";
+
+const loading = () => {
+  return (
+    <div className=" flex justify-center items-center h-screen">
+        <ImSpinner9 className=" text-xl"/>           
+    </div>
+  );
+};
+
+export default loading;
