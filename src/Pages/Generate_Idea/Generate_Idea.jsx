bc9691e210aac983bfb329a82fd9de8c5845ff10
unknown <redoyhasan4585@gmail.com>
2024-08-10 22:41:28 +0400

    working on generate idea page added route

--- src/Pages/Generate_Idea/Generate_Idea.jsx
+++ src/Pages/Generate_Idea/Generate_Idea.jsx
@@ -1,9 +1,23 @@
+'use client'
 import Image from 'next/image';
 import bannerImage from '../../../public/generateIdea/ai-cloud-concept-with-robot-arm.jpg'
 import { FaLocationArrow } from 'react-icons/fa';
 import aiIcon from '../../../public/generateIdea/chatbot-0.png'
+import useAxios from '@/hooks/useAxios';
+import axios from 'axios';
+import { useState } from 'react';
 
 const Generate_Idea = () => {
+  const [promptInput , setPromptInput]=useState('')
+  // get axios 
+  const axiosHook= useAxios()
+  const handlePrompt= async(e)=>{
+    e.preventDefault()
+    const prompt=e.target.text.value
+    console.log(prompt);
+    const res = await axiosHook.post('/api/generate_idea',{prompt})
+    console.log(res.data);
+  }
   return (
     <main className='my-28 max-w-[95%] overflow-hidden lg:max-w-[85%] mx-auto flex gap-10 '>
       {/* image section  */}
@@ -14,13 +28,13 @@ const Generate_Idea = () => {
       <section>
          {/* prompt div  */}
          <div className='space-y-5 mx-auto'>
-            <button className='btn-prompt'>How to Build A Chicken Farm?</button>
+            <button onClick={()=>setPromptInput('How to Build A Chicken Farm')} className='btn-prompt'>How to Build A Chicken Farm?</button>
             
-            <button className='btn-prompt'>How can I optimize my chicken farm?</button>
-            <button className='btn-prompt'>What is the best time to start a Chicken farm?</button>
-            <button className='btn-prompt'>How can I build a chicken feed producing factory?</button>
-            <button className='btn-prompt'>How much money do I need to build a medium poultry farm ?</button>
-            <button className='btn-prompt'>How much money do I need to build a medium poultry feed factory?</button>
+            <button onClick={()=>setPromptInput('How can I optimize my chicken farm')} className='btn-prompt'>How can I optimize my chicken farm?</button>
+            <button onClick={()=>setPromptInput('What is the best time to start a Chicken farm')} className='btn-prompt'>What is the best time to start a Chicken farm?</button>
+            <button onClick={()=>setPromptInput('How can I build a chicken feed producing factory')} className='btn-prompt'>How can I build a chicken feed producing factory?</button>
+            <button onClick={()=>setPromptInput('How much money do I need to build a medium poultry farm ')} className='btn-prompt'>How much money do I need to build a medium poultry farm ?</button>
+            <button onClick={()=>setPromptInput('How much money do I need to build a medium poultry feed factory')} className='btn-prompt'>How much money do I need to build a medium poultry feed factory?</button>
 
          </div>
  {/* input section  */}
@@ -28,14 +42,18 @@ const Generate_Idea = () => {
      <div className=' ='>
      <Image src={aiIcon} width={50} height={50} alt='ai chatbot icon' className=''></Image>
      </div>
-     <div className="join flex-1">
+      <form onSubmit={handlePrompt}>
+      <div className="join flex-1">
        
        <input
          type="text"
+         name='text'
+         defaultValue={promptInput}
          placeholder="Ask Anything"
          className=" px-3 border border-gray-400 outline-none join-item w-full min-w-full " />
-       <button className="btn border-2 btn-primary join-item  flex items-center gap-2">Ask<FaLocationArrow className=' -rotate-45'></FaLocationArrow></button>
-     </div>      
+       <button type='submit' className="btn border-2 btn-primary join-item  flex items-center gap-2">Ask</button>
+     </div>  
+      </form>
             </section> 
       </section>
     </main>
--- src/app/api/generate_idea/route.js
+++ src/app/api/generate_idea/route.js
@@ -1,3 +1,24 @@
+import { NextResponse } from "next/server";
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 export const POST = async(request)=>{
+  const {prompt} =await request.json()
+
+try {
+  const result = await model.generateContent(prompt);
+  const resp = await result.response;
+  const text = response.text();
+  console.log(text);
+  return NextResponse.json({text})
+} catch (error) {
+  return NextResponse.json({error})
   
+}
+
+
 }
