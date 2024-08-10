3e1d6f413c6e03c5b45fc8a0865823df3f4414ab
unknown <redoyhasan4585@gmail.com>
2024-08-10 12:54:02 +0400

    adding meta data

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,10 @@
 import Login from '@/Pages/Login/Login';
 import Register from '@/Pages/Register/Register';
 import React from 'react';
-
+export const metadata = {
+  title: "Register",
+  description: "poultry hub register page, ",
+};
 const RegisterPage = () => {
   return (
     <div>
--- src/app/reviews/page.jsx
+++ src/app/reviews/page.jsx
@@ -1,6 +1,10 @@
 import Reviews from "@/Pages/Reviews/Reviews";
-
+  export const metadata = {
+    title: "Reviews",
+    description: "poultry hub reviews page, in this page user can look what poultry hub's users says about them",
+  };
 const ReviewsPage = () => {
+
   return (
     <main className=" min-h- my-20 max-w-[95%] overflow-hidden lg:max-w-[90%] mx-auto mt-[54px] lg:mt-[70px]  ">
       <Reviews></Reviews>
